ef83a5fd587689b319e5803f7e67164b78b7828b
Marji Symonds <symondsm@oregonstate.edu>
2020-10-18 13:38:55 -0700

    Add through exploration part 5

--- bin/main.dart
+++ bin/main.dart
@@ -1,34 +1,52 @@
-// import 'package:space_adventure/space_adventure.dart' as space_adventure;
-import 'dart:io';
+import 'package:space_adventure/dart_space_adventure.dart';
 
 void main(List<String> arguments) {
-  print('Welcome to the Solar System!\n'
-      'There are 8 planets to explore.\n'
-      'What is your name?');
+  SpaceAdventure(
+      planetarySystem: PlanetarySystem(
+        name: 'Solar System', 
+        planets: mockPlanets()
+        )
+  ).start();
+}
 
-  final name = stdin.readLineSync();
+List<Planet> mockPlanets() {
+  return [
+    Planet(
+        name: 'Mercury',
+        description: 'A very hot planet, closest to the sun.'
+    ),
+    Planet(
+        name: 'Venus',
+        description: 'It\'s very cloudy here!'
+    ),
+    Planet(
+        name: 'Earth',
+        description: 'There is something very familiar about this planet.'
+    ),
+    Planet(
+        name: 'Mars',
+        description: 'Known as the red planet.'
+    ),
+    Planet(
+        name: 'Jupiter',
+        description: 'A gas giant, with a noticeable red spot.'
+    ),
+    Planet(
+        name: 'Saturn',
+        description: 'This planet has beautiful rings around it.'
+    ),
+    Planet(
+        name: 'Uranus',
+        description: 'Strangely, this planet rotates around on its side.'
+    ),
+    Planet(
+        name: 'Neptune',
+        description: 'A very cold planet, furthest from the sun.'
+    ),
+    Planet(
+        name: 'Pluto',
+        description: 'I don\'t care what they say - it\'s a planet.'
+    )
+  ];
 
-  print('Nice to meet you, $name.'
-      ' My name is Eliza, I\'m an old friend of Alexa.\n'
-      'Let\s go on an adventure!\n'
-      'Shall I randomly choose a planet for you to visit? (Y or N)');
-
-  String answer;
-
-  while (answer != 'Y' && answer != 'N') {
-    answer = stdin.readLineSync();
-    if (answer == 'Y') {
-      print('Traveling to Mercury...\n'
-          'Arrived at Mercury. A very cold planet, furthest from the sun.');
-    } else if (answer == 'N') {
-      print('Name the planet you would like to visit.');
-
-      final planetName = stdin.readLineSync();
-
-      print('Traveling to $planetName...\n'
-          'Arrived at $planetName. A very cold planet, furthest from the sun.');
-    } else {
-      print('Sorry, I didn\'t get that.');
-    }
-  }
 }
